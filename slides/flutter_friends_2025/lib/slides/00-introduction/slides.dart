915886a4a1b15916963ce679c4276737ea462bc6
Roaa94 <roaa94khaddam@gmail.com>
2025-09-01 05:09:58 +0200

    adjust introduction slides

--- slides/flutter_friends_2025/lib/slides/00-introduction/slides.dart
+++ slides/flutter_friends_2025/lib/slides/00-introduction/slides.dart
@@ -21,15 +21,15 @@ final introductionSlides = <FlutterDeckSlideWidget>[
   // 3
   ImageSlide(
     title: 'The Seven Bridges of Königsberg',
-    path: 'assets/images/city-illustration-with-graph.png',
+    path: 'assets/images/city-illustration-with-graph-info-1.png',
     route: 'city-illustration-with-graph',
     width: 800,
   ),
   // 4
   ImageSlide(
     title: 'The Seven Bridges of Königsberg',
-    path: 'assets/images/city-illustration-with-graph-degrees.png',
-    route: 'city-illustration-with-graph-degrees',
+    path: 'assets/images/city-illustration-with-graph-info.png',
+    route: 'city-illustration-with-graph-info',
     width: 800,
   ),
   // 5
@@ -39,8 +39,22 @@ final introductionSlides = <FlutterDeckSlideWidget>[
     route: 'konigsberg-present',
     width: 700,
   ),
+  // 6
+  ImageSlide(
+    title: 'Present State of Königsberg - Kaliningrad, Russia',
+    path: 'assets/images/city-illustration-with-graph-new.png',
+    route: 'konigsberg-present-graph',
+    width: 700,
+  ),
+  // 7
   SectionTitleSlide(
-    'Graph Theory',
+    'The Geometry of Position',
+    subtitle: '',
+  ),
+  // 8
+  SectionTitleSlide(
+    'The Geometry of Position',
+    subtitle: 'Graph Theory',
     route: 'graph-theory',
   ),
 ];
